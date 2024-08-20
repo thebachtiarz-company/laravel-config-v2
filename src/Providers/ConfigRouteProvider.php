4edd8f0da72301eb93ddfde51e1df1600e666db9
thebachtiarz <bachtiarptibinlppnri2417@gmail.com>
2024-08-20 09:02:15 +0700

    feat(config): route register

--- src/Providers/ConfigRouteProvider.php
+++ src/Providers/ConfigRouteProvider.php
@@ -2,13 +2,12 @@
 
 namespace TheBachtiarz\Config\Providers;
 
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
 use Illuminate\Support\Facades\Route;
 
-class ConfigRouteProvider extends RouteServiceProvider
+class RouteServiceProvider extends \Illuminate\Foundation\Support\Providers\RouteServiceProvider
 {
     public function boot(): void
     {
-        Route::prefix('system')->group(__DIR__ . '/../routes/api.php');
+        Route::prefix('system')->middleware('api')->group(__DIR__ . '/../routes/api.php');
     }
 }
